7b53399806b4039d8cf6ebf1d5937c31de2b230d
Lucas Gebara <lucasgebara@outlook.com>
2019-06-20 11:43:28 -0300

    meta tags deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 
   def after_sign_in_path_for(resource)
     dashboard_path
